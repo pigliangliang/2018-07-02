6c18e4d81ed5b0502571be3b1731b917808542f7
zhuxiaoliang <836904717@qq.com>
2018-07-03 11:03:09 +0800

    init code

--- python_GC.py
+++ python_GC.py
@@ -19,11 +19,13 @@ print('b：',sys.getrefcount(b))
 a refcount:2
 b： 3
 """
-
-a = "hi pig"
+#这里变量a是一个字符串，列表或者其他整型值，其引用数不同，为啥？？？？？？一万个问号
+a = ""
 b = []
 #a.append(b)
-print('a refcount:{}'.format(sys.getrefcount(a)))
+print('a refcount:{}'.format(sys.getrefcount(a)))#输出189
+a = 'ddfdf'
+print(sys.getrefcount(a))#输出4
 print(gc.collect())
 print(gc.garbage)
 print('b----',sys.getrefcount(b))
@@ -40,3 +42,4 @@ a = 'zhu'
 b = 'zhu'
 print(a is b)
 #输入 True
+print(sys.getrefcount(a))
